225c189b9da9ccc87edada60fafde3135885a38c
Elias <mail@elias-dieter.de>
2023-12-21 11:27:11 +0100

    adding hashing and salting

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,8 +2,10 @@ import express from "express";
 import SessionJs from "sessionjs";
 import multer from "multer";
 import path from "path";
+import bcrypt from "bcrypt";
 import DatabaseController from "../database/DatabaseController.js";
 
+const saltRounds = 10;
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -22,34 +24,32 @@ let session = new SessionJs();
 
 adminRouter.post("/login", async (req, res) => {
     let username = req.body.username;
-
     const users = await dbc.getUser(username);
-    
-    if(users.length == 0){
+
+    if (users.length == 0) {
         console.log("Failed to login");
         res.json({
             status: 'error',
             message: 'Failed to login'
         });
-    }
-
-    else{
+    } else {
         users.forEach((user) => {
-            if(user.password == req.body.password){
-                let sessionId = session.start(user);
-                res.cookie('sessionId', sessionId);
-                res.json({
-                    status: 'ok',
-                    data: sessionId
-                });
-            }
-            else{
-                console.log("Failed to login");
-                res.json({
-                    status: 'error',
-                    message: 'Failed to login'
-                });
-            }
+            bcrypt.compare(req.body.password, user.password, async (err, result) => {
+                if (result) {
+                    let sessionId = session.start(user);
+                    res.cookie('sessionId', sessionId);
+                    res.json({
+                        status: 'ok',
+                        data: sessionId
+                    });
+                } else {
+                    console.log("Failed to login");
+                    res.json({
+                        status: 'error',
+                        message: 'Failed to login'
+                    });
+                }
+            });
         });
     }
 });
@@ -82,21 +82,29 @@ adminRouter.post("/updateUser", (req, res) => {
     let sessionId = session.getSessionId(thisSession);
     const user = session.getUser(sessionId);
 
-    newUsername ? newUsername : newUsername = user.username;
-    password ? password : password = user.password;
+    newUsername = newUsername || user.username;
+    password = password || user.password;
 
+    bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
+        if (err) {
+            console.error(err);
+            res.json({
+                status: 'error'
+            });
+        } else {
+            const newUser = {
+                id: user.id,
+                username: newUsername,
+                password: hashedPassword
+            };
 
-    const newUser = {
-        id: user.id,
-        username: newUsername,
-        password: password
-    };
+            let check = session.updateUser(sessionId, newUser);
+            dbc.updateUser(newUser);
 
-    let check = session.updateUser(sessionId, newUser);
-    dbc.updateUser(newUser);
-
-    res.json({
-        status: 'ok'
+            res.json({
+                status: 'ok'
+            });
+        }
     });
 });
 
